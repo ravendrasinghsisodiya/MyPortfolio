7a5be18a40daf613ff98106fb79d6bde707a3093
ravendrasinghsisodia <ravendrasinghsisodia@gmail.com>
2023-08-21 01:16:08 +0530

    add email in views

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib import auth, messages
+from django.contrib import messages
+from django.core.mail import send_mail
 from .models import *
+from django.conf import settings
 
 
 def home(Request):
@@ -16,9 +18,14 @@ def contact(Request):
         c.subject = Request.POST.get("subject")
         c.message = Request.POST.get("message")
         c.save()
-        messages.success(
-            Request, "Thanks To Share Your Query With Us!!! I'm Contact You Soon")
-    return HttpResponseRedirect(Request,"/contact/")
+        subject = 'Thanks to contact us : Ravendra Singh'
+        message =   "Hello sir/madam "+c.name+"\nThank you to contact us\nI'm contact you soon\nRavendra Singh"
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [c.email, ]
+        send_mail( subject, message, email_from, recipient_list )
+        return HttpResponseRedirect("#contact")
+        
+    return HttpResponseRedirect(Request,"#contact")
 
 
 def about(Request):
