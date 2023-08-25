192af93dd4e13d3dd88222d81680ae8b2350f83f
Ravendra Singh <ravendrasingh1357@gmail.com>
2023-08-25 18:42:29 +0000

    20

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -115,9 +115,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = '/home/ravendra07/MyPortfolio/static/'
 =======
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True  
-EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'ravendrasingh1357@gmail.com'  
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ravendrasingh1357@gmail.com'
 EMAIL_HOST_PASSWORD = 'ucqtupvkzbgpoyxq'
 >>>>>>> 3941d27a8b4016257211080c1edf444e5d351249
