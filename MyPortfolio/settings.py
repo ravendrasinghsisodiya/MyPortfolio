3941d27a8b4016257211080c1edf444e5d351249
ravendrasinghsisodia <ravendrasinghsisodia@gmail.com>
2023-08-25 23:01:58 +0530

    25/08/2023

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -109,4 +109,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'ravendrasingh1357@gmail.com'  
-EMAIL_HOST_PASSWORD = 'ucqtupvkzbgpoyxq'  
+EMAIL_HOST_PASSWORD = 'ucqtupvkzbgpoyxq'
